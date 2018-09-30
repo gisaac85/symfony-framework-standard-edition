bf9df99394cdeae9bb401ee31359a83a9a77bce3
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-30 18:01:55 +0200

    Doctrine database

--- src/Zenva/WorkoutBundle/Controller/DefaultController.php
+++ src/Zenva/WorkoutBundle/Controller/DefaultController.php
@@ -5,18 +5,27 @@ namespace Zenva\WorkoutBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/workout")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Route("/hello/")
+     * @Route("/",name="workout-index")
+     * @Template()
     */
-    public function indexAction($name='Gaorieh')
+    public function indexAction()
     {
-      $res=new Response(json_encode(array('name'=>$name)));
-      $res->headers->set('Content-Type','application/json');
-      return $res;
+    $workouts=[
+       ['date'=>new \DateTime(),'activity'=>'walking','hours'=>1],
+       ['date'=>new \DateTime(),'activity'=>'swiming','hours'=>1.5],
+       ['date'=>new \DateTime(),'activity'=>'Gym','hours'=>2],
+       ['date'=>new \DateTime(),'activity'=>'Tv Watching','hours'=>3],    
+        
+    ];
+    return ['workouts'=>$workouts,'name'=>'Your Name','age'=>99];
     }
 }
---
+++ src/Zenva/WorkoutBundle/Entity/Workout.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Zenva\WorkoutBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Workout
+ *
+ * @ORM\Table(name="workout")
+ * @ORM\Entity(repositoryClass="Zenva\WorkoutBundle\Repository\WorkoutRepository")
+ */
+class Workout
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="occurrenceDate", type="datetime")
+     */
+    private $occurrenceDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="activity", type="string", length=255)
+     */
+    private $activity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set occurrenceDate
+     *
+     * @param \DateTime $occurrenceDate
+     *
+     * @return Workout
+     */
+    public function setOccurrenceDate($occurrenceDate)
+    {
+        $this->occurrenceDate = $occurrenceDate;
+
+        return $this;
+    }
+
+    /**
+     * Get occurrenceDate
+     *
+     * @return \DateTime
+     */
+    public function getOccurrenceDate()
+    {
+        return $this->occurrenceDate;
+    }
+
+    /**
+     * Set activity
+     *
+     * @param string $activity
+     *
+     * @return Workout
+     */
+    public function setActivity($activity)
+    {
+        $this->activity = $activity;
+
+        return $this;
+    }
+
+    /**
+     * Get activity
+     *
+     * @return string
+     */
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+}
+
